796287a81d269b601436bf0c1b574aa5faad4a4a
Painted-Black <syzygy.sys@gmail.com>
2021-05-07 17:44:13 +0300

    Add open syllable test

--- src/TTS/tests/syllable_unit_test.py
+++ src/TTS/tests/syllable_unit_test.py
@@ -3,17 +3,18 @@ import unittest
 from modules.phonetic_processor.syllabic_complex1_unit import SyllabicComplex1Unit
 from modules.phonetic_processor.syllabic_complex2_unit import SyllabicComplex2Unit
 from modules.phonetic_processor.syllabic_complex3_unit import SyllabicComplex3Unit
+from modules.phonetic_processor.open_syllable_unit import OpenSyllableUnit
 from modules.allophone import Allophone
 from modules.phonetic_processor.phoneme_allophone_transcriptor import PhonemeAllophoneTranscriptor
 from modules.phonetic_processor.letter_phoneme_transcriptor import LetterPhonemeTranscriptor
 
 
-def run_test_cases(__scu, _allophones: [[Allophone]], _results: []):
+def run_test_cases(method, _allophones: [[Allophone]], _results: []):
     n = len(_allophones)
-    for i in range(n):
-        allophones = _allophones[i]
-        results = _results[i]
-        res = __scu.process_by_word(allophones)
+    for q in range(n):
+        allophones = _allophones[q]
+        results = _results[q]
+        res = method(allophones)
         assert len(res) == len(results)
         n = len(res)
         for i in range(n):
@@ -28,6 +29,57 @@ def run_test_cases(__scu, _allophones: [[Allophone]], _results: []):
                 k = len(syllable)
                 for g in range(k):
                     assert str(res[i][j][g].phoneme) == results[i][j][g]
+
+
+class OpenSyllableUnitCase(unittest.TestCase):
+    def setUp(self):
+        self.__osu = OpenSyllableUnit()
+        self.__lft = LetterPhonemeTranscriptor()
+        self.__pat = PhonemeAllophoneTranscriptor()
+
+    def test_open_syllables(self):
+        texts = ["КО+ЛОСА", "КО+Л", "", "О+", "К", "О+М", "МО+",
+                 "МНО+ГО", "МАЙК", "А+ММО"]
+        allophones = []
+        for text in texts:
+            phonemes = self.__lft.transcript(text)
+            cur_allophones = self.__pat.transcript(phonemes)
+            allophones.append(cur_allophones)
+        results = [[["k", "o"], ["l", "a"], ["s", "a"]],
+                   [["k", "o", "l"]],
+                   [[]],
+                   [["o"]],
+                   [["k"]],
+                   [["o", "m"]],
+                   [["m", "o"]],
+                   [["m", "n", "o"], ["g", "a"]],
+                   [["m", "a", "j'", "k"]],
+                   [["a"], ["m", "m", "a"]],
+                   ]
+        try:
+            self.run_test_cases(allophones, results)
+        except AssertionError:
+            print("Test {0} failed".format("test_open_syllables"))
+            raise AssertionError
+
+    def run_test_cases(self, _allophones: [[Allophone]], _results: []):
+        n = len(_allophones)
+        for q in range(n):
+            allophones = _allophones[q]
+            results = _results[q]
+            res = self.__osu.process(allophones)
+            assert len(res) == len(results)
+            n = len(res)
+            for i in range(n):
+                unit = res[i]
+                real_unit = results[i]
+                assert len(unit) == len(real_unit)
+                k = len(unit)
+                for g in range(k):
+                    assert str(res[i][g].phoneme) == results[i][g]
+
+    def tearDown(self):
+        pass
 
 
 class SyllableComplex3UnitCase(unittest.TestCase):
@@ -50,7 +102,7 @@ class SyllableComplex3UnitCase(unittest.TestCase):
                    [[["k", "a"], ["l", "a"]]]
                    ]
         try:
-            run_test_cases(self.__sc3u, allophones, results)
+            run_test_cases(self.__sc3u.process_by_word, allophones, results)
         except AssertionError:
             print("Test {0} failed".format("test_sonant_vowel_by_word"))
             raise AssertionError
@@ -80,7 +132,7 @@ class SyllableComplex2UnitCase(unittest.TestCase):
                    [[["a", "r", "m", "a"], ["d", "a"]]]
                    ]
         try:
-            run_test_cases(self.__sc2u, allophones, results)
+            run_test_cases(self.__sc2u.process_by_word, allophones, results)
         except AssertionError:
             print("Test {0} failed".format("test_two_sonants_by_word"))
             raise AssertionError
@@ -106,7 +158,7 @@ class SyllableComplex1UnitCase(unittest.TestCase):
                    [[["a", "j'", "a", "k", "s"]]]
                    ]
         try:
-            run_test_cases(self.__sc1u, allophones, results)
+            run_test_cases(self.__sc1u.process_by_word, allophones, results)
         except AssertionError:
             print("Test {0} failed".format("test_single_vowel_syllable"))
             raise AssertionError
@@ -125,7 +177,7 @@ class SyllableComplex1UnitCase(unittest.TestCase):
                      ["g", "a"], ["c", "y", "j'", "a"]]]
                    ]
         try:
-            run_test_cases(self.__sc1u, allophones, results)
+            run_test_cases(self.__sc1u.process_by_word, allophones, results)
         except AssertionError:
             print("Test {0} failed".format("test_ja"))
             raise AssertionError
@@ -144,7 +196,7 @@ class SyllableComplex1UnitCase(unittest.TestCase):
                      ["g", "a"], ["c", "y", "j'", "a"]]]
                    ]
         try:
-            run_test_cases(self.__sc1u, allophones, results)
+            run_test_cases(self.__sc1u.process_by_word, allophones, results)
         except AssertionError:
             print("Test {0} failed".format("test_vowels_sequence_by_word"))
             raise AssertionError
