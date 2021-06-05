02d2811ae3a1e3f2731b97967b30d6dea5b98188
Painted-Black <syzygy.sys@gmail.com>
2021-06-05 16:42:07 +0300

    Add run scripts

--- src/TTS/dist.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install python3-pip
-
-sudo apt-get install -y libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev libzstd-dev liblz4-dev
-git clone https://github.com/facebook/rocksdb.git
-cd rocksdb/
-make all
-
-sudo pip3 install -r requirements.txt
